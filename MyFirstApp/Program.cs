88b5c83adabbf0c2fd8ed9ca62c9215f012ca14a
MarkusT22 <markus.tammoja@tktk.ee>
2024-03-20 19:24:25 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// This is a comment to my code
+
+//Rakendus küsib kasutajalt sisestada tema nime
+//Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+
+Console.WriteLine($"Hello, {userName}!"); //Output
